083e3dfa99af68853b93fc27cde78de856978304
Lukas Spieß <lspiess@microsoft.com>
2016-01-11 20:05:13 +0100

    Add BITUpdateManagerTests

---
+++ Support/HockeySDKTests/BITUpdateManagerTests.m
@@ -0,0 +1,43 @@
+//
+//  BITUpdateManagerTests.m
+//  HockeySDK
+//
+//  Created by Lukas Spieß on 11/01/16.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+#import <OCHamcrestIOS/OCHamcrestIOS.h>
+#import <OCMockitoIOS/OCMockitoIOS.h>
+
+#import "BITUpdateManager.h"
+
+@interface BITUpdateManagerTests : XCTestCase
+
+@end
+
+@implementation BITUpdateManagerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testReportError {
+  
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
